4f29f41fe88224410e0157333a03be76c8451a41
Luisf66 <Luisf311220@gmail.com>
2026-03-18 09:48:42 -0300

    feat: Implementação de user #5

--- users/views.py
+++ users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from drf_spectacular.utils import extend_schema
+
+from .serializer import UserSerializer
+from .models import User
 
 # Create your views here.
+@extend_schema(tags=["Users"])
+class UserListCreateView(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+@extend_schema(tags=["Users"])
+class UserRetrieveUpdateView(generics.RetrieveUpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
